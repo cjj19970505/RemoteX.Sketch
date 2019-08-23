f53986ffc3113f28d814dd75efef9d916798ed58
陈俊嘉 <cjj19970505@live.cn>
2019-08-23 22:45:25 +0800

    KeyboardButton写一半

--- RemoteX.Sketch/InputComponent/Button.cs
+++ RemoteX.Sketch/InputComponent/Button.cs
@@ -1,10 +1,78 @@
-﻿using System;
+﻿using RemoteX.Sketch.CoreModule;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace RemoteX.Sketch.InputComponent
 {
-    class Button
+    public abstract class Button : SketchObject, IInputComponent
     {
+        public int Level { get; set; }
+
+        public RectTransform RectTransform { get; private set; }
+        public virtual IArea StartRegion { get; }
+        public SketchInputManager SketchInputManager { get; private set; }
+
+        readonly List<SketchPointer> OnSketchPointerList;
+
+        public bool Pressed { get; private set; }
+        public Button():base()
+        {
+            Pressed = false;
+            OnSketchPointerList = new List<SketchPointer>();
+        }
+
+        protected override void OnInstantiated()
+        {
+            base.OnInstantiated();
+            var sketchInfo = SketchEngine.FindObjectByType<SketchInfo>();
+            RectTransform = new RectTransform(sketchInfo);
+            SketchInputManager = SketchEngine.FindObjectByType<SketchInputManager>();
+            SketchInputManager.PointerMoved += SketchInputManager_PointerMoved;
+            SketchInputManager.PointerReleased += SketchInputManager_PointerReleased;
+        }
+
+        private void SketchInputManager_PointerReleased(object sender, SketchPointer e)
+        {
+            if(OnSketchPointerList.Contains(e))
+            {
+                OnReleased();
+            }
+        }
+        private void SketchInputManager_PointerMoved(object sender, SketchPointer e)
+        {
+            if(OnSketchPointerList.Contains(e))
+            {
+                if(!StartRegion.IsOverlapPoint(e.Point))
+                {
+                    OnSketchPointerList.Remove(e);
+                    if(OnSketchPointerList.Count == 0)
+                    {
+                        OnReleased();
+                    }
+                }
+            }
+            else
+            {
+                if (e.HitLayer == Level && !StartRegion.IsOverlapPoint(e.Point))
+                {
+                    OnSketchPointerList.Add(e);
+                    if(OnSketchPointerList.Count == 1)
+                    {
+                        OnPressed();
+                    }
+                }
+            }
+        }
+
+        public virtual void OnReleased()
+        {
+
+        }
+
+        public virtual void OnPressed()
+        {
+
+        }
     }
 }
---
+++ RemoteX.Sketch/InputComponent/KeyboardKeyButton.cs
@@ -0,0 +1,48 @@
+﻿using RemoteX.Sketch.Skia;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace RemoteX.Sketch.InputComponent
+{
+    public class KeyboardKeyButton:Button, ISkiaRenderer
+    {
+        public override IArea StartRegion
+        {
+            get
+            {
+                return new RectArea(RectTransform.Rect);
+            }
+        }
+
+        public event EventHandler KeyDown;
+        public event EventHandler KeyUp;
+        public override void OnPressed()
+        {
+            base.OnPressed();
+            KeyDown?.Invoke(this, null);
+        }
+        public override void OnReleased()
+        {
+            base.OnReleased();
+            KeyUp?.Invoke(this, null);
+        }
+
+        SKPaint ReleasedPaint = new SKPaint()
+        {
+            Style = SKPaintStyle.Fill,
+            Color = SKColors.Blue
+        };
+        SKPaint PressedPaint = new SKPaint()
+        {
+            Style = SKPaintStyle.Fill,
+            Color = SKColors.Red
+        };
+        public void PaintSurface(SkiaManager skiaManager, SKCanvas canvas)
+        {
+            
+        }
+    }
+}
